5cb0e2e917efdfa5168686e9729d3c3f56360a00
Eduardo Damasceno <eduardo@talentacademy.com.br>
2025-05-18 11:25:09 -0300

    feat: ✨ Content-driven web application foundations

--- src/app/(frontend)/posts/[slug]/page.tsx
+++ src/app/(frontend)/posts/[slug]/page.tsx
@@ -1,13 +1,10 @@
 import { sanityFetch } from '@/sanity/lib/live';
 import { POST_QUERY } from '@/sanity/lib/queries';
+import { Post } from '@/components/Post';
 import { notFound } from 'next/navigation';
-import Link from 'next/link';
 
 export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
-    const { data: post } = await sanityFetch({
-        query: POST_QUERY,
-        params: await params,
-    });
+    const { data: post } = await sanityFetch({ query: POST_QUERY, params: await params });
 
     if (!post) {
         notFound();
@@ -15,9 +12,7 @@ export default async function Page({ params }: { params: Promise<{ slug: string 
 
     return (
         <main className="container mx-auto grid grid-cols-1 gap-6 p-12">
-            <h1 className="text-4xl font-bold text-balance">{post?.title}</h1>
-            <hr />
-            <Link href="/posts">&larr; Return to index</Link>
+            <Post {...post} />
         </main>
     );
 }
